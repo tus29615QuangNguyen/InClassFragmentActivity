fa8b3438b54eb1f5ff2659ed3c7958aa6226fea6
tus29615QuangNguyen <tus29615@temple.edu>
2024-10-15 12:09:03 -0400

    Fragment2Activity

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
+        val fragment1 = ImageDisplayFragment.newInstance(imageArray)
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView,fragment1).commit()
 
     }
 }
